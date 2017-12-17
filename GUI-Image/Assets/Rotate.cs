bfbd7229fce2b1f93b7d243d1f07f92838bcf3a2
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2017-12-17 00:26:10 -0700

    GUI_Imager

---
+++ GUI-Image/Assets/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotate : MonoBehaviour {
+
+    public Button yourButton;
+
+    public Transform Image;
+
+    int speed = -5;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        Image.transform.Rotate(0, 0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
